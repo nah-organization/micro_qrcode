64243b2db77d2f6b8f9fec53a2888b5627c20ad5
8128-33550336 <66201155+8128-33550336@users.noreply.github.com>
2022-12-26 14:15:40 +0900

    mainを切り出した.

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,30 @@
-import http from 'http';
+import http, { OutgoingHttpHeaders } from 'http';
 import QRCode from 'qrcode-terminal';
 import { topRedirect } from './envs';
 
-export function start() {
-    const server = http.createServer((req, res) => {
-        const url = new URL(req.url ?? '/', 'http://localhost/');
-        const target = url.searchParams.get('url');
-        if (!target) {
-            res.writeHead(302, {
-                'Location': topRedirect
-            });
-            res.end(`See: ${topRedirect}`);
-            return;
-        }
-        QRCode.generate(target, { small: true }, (str) => {
-            res.writeHead(200, {
-                'Content-Type': 'text/plain; charset=UTF-8'
-            });
-            res.end(str);
+export function main(req: { url?: string; }, res: {
+    end(value: string): void,
+    writeHead(status: number, header: OutgoingHttpHeaders): void;
+}) {
+    const url = new URL(req.url ?? '/', 'http://localhost/');
+    const target = url.searchParams.get('url');
+    if (!target) {
+        res.writeHead(302, {
+            'Location': topRedirect
         });
+        res.end(`See: ${topRedirect}`);
+        return;
+    }
+    QRCode.generate(target, { small: true }, (str) => {
+        res.writeHead(200, {
+            'Content-Type': 'text/plain; charset=UTF-8'
+        });
+        res.end(str);
     });
+};
+
+export function start() {
+    const server = http.createServer(main);
 
     server.listen('/socket/server.sock', () => {
         console.log(`Server running`);
