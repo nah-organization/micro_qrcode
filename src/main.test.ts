3d55bcd7017cfe11c851f09cb1f7598bd81c7d24
8128-33550336 <66201155+8128-33550336@users.noreply.github.com>
2022-12-26 14:28:03 +0900

    add start test

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,7 +1,7 @@
 import http from 'http';
 import QRCode from 'qrcode-terminal';
 
-import { start } from "./main";
+import { start, main } from "./main";
 jest.mock('http');
 jest.mock('qrcode-terminal');
 jest.mock('./envs', () => {
@@ -17,15 +17,20 @@ const httpMock = http as jest.Mocked<typeof http>;
 
 describe('server test', () => {
     beforeAll(() => {
-        httpMock.createServer.mockReturnValue({ listen: jest.fn() } as unknown as ReturnType<typeof http.createServer>);
     });
-    describe('when starting', () => {
+    describe('test start', () => {
+        const mockServer = { listen: jest.fn() };
         beforeAll(() => {
+            httpMock.createServer.mockReturnValue(mockServer as unknown as ReturnType<typeof http.createServer>);
             start();
         });
         test('createServerOption', () => {
             expect(httpMock.createServer).toHaveBeenCalledTimes(1);
-            expect(httpMock.createServer).toHaveBeenCalledWith(expect.any(Function));
+            expect(httpMock.createServer).toHaveBeenCalledWith(main);
+        });
+        test('listen', () => {
+            expect(mockServer.listen).toHaveBeenCalledTimes(1);
+            expect(mockServer.listen).toHaveBeenCalledWith('/socket/server.sock', expect.any(Function));
         });
     });
 });
